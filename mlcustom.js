ca7f6d4d408bf879b01456d900c727ccc884b561
Subhashis Suara <subhashissuara@gmail.com>
2023-07-06 17:58:50 +0530

    Update

--- mlcustom.js
+++ mlcustom.js
@@ -1,1 +1,9 @@
-console.log("MLCUSTOM: mlcustom.js loaded");
+console.log("mlcustom.js loaded");
+
+// Set form progress indicator numbers
+var formProgressIndicators = document.querySelectorAll('.form-progress-indicator');
+
+formProgressIndicators.forEach(function(formProgressIndicator) {
+  var number = parseInt(formProgressIndicator.className.match(/\d+/)[0]);
+  formProgressIndicator.textContent = number;
+});
